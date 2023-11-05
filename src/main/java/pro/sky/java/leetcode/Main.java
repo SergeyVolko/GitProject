8201eac6e5e52648b4b71fef7de2c04d1fb7af52
Lenovo <rusobraz@mail.ru>
2023-11-05 20:17:46 +0300

    Test

--- src/main/java/pro/sky/java/leetcode/Main.java
+++ src/main/java/pro/sky/java/leetcode/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Выпоолнили новую задачу");
         System.out.println("Своя задача");
+        System.out.println();
     }
 }
